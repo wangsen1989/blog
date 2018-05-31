8541046bad967c01a92c583e67561154b893f0d3
wangsen <295656753@qq.com>
2018-05-31 23:05:56 +0800

    record页面布局ok

--- src/client/components/AddRecord.jsx
+++ src/client/components/AddRecord.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { Switch, Button, List, InputItem, WhiteSpace } from 'antd-mobile';
+import { Switch, Button, List, InputItem, WhiteSpace, TextareaItem } from 'antd-mobile';
 import { createForm } from 'rc-form';
 import { changeMyListVisible } from '../redux/action/record.action'
 
@@ -15,52 +15,39 @@ import { changeMyListVisible } from '../redux/action/record.action'
 class AddRecord extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            title: '',
-            content: ''
-        }
-        this.titleChange = this.titleChange.bind(this)
-        this.contentChange = this.contentChange.bind(this)
         this.submit = this.submit.bind(this)
     }
-    titleChange(val) {
-        this.setState({ title: val })
-    }
-    contentChange(val) {
-        this.setState({ content: val })
-    }
     submit() {
-        console.log(this.state)
+        console.log(this.props.form.getFieldsValue())
         this.props.changeMyListVisible(true)
     }
     render() {
         const { getFieldProps } = this.props.form;
-        const { title, content } = this.state;
         return (
-            <List>
-                <WhiteSpace/>
-                <InputItem
-                    {...getFieldProps('title')}
-                    type="text"
-                    // clear={true}
-                    value={title}
-                    onChange={this.titleChange}
-                >标题</InputItem>
-                <WhiteSpace />
-                <InputItem
-                    {...getFieldProps('content')}
-                    type="text"
-                    // clear={true}
-                    value={content}
-                    onChange={this.contentChange}
-                >内容</InputItem>
-                <WhiteSpace />
+            <div>
+                <List renderHeader={() => '标题'}>
+                    <InputItem
+                        {...getFieldProps('title')}
+                        type="text"
+                        clear={true}
+                    ></InputItem>
+                </List>
+
+                <List renderHeader={() => '内容'}>
+                    <TextareaItem
+                        {...getFieldProps('content', {
+                            initialValue: '',
+                        })}
+                        rows={14}
+                    // count={10000}
+                    />
+                </List>
                 <Button
                     type="primary"
                     onClick={this.submit}
                 >发表</Button>
+            </div>
 
-            </List>
         );
     }
 }
