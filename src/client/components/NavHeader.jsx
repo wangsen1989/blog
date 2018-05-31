510c3a8e9c0f9af46006ce1ead11be5ae66111f8
wangsen <295656753@qq.com>
2018-05-31 19:31:41 +0800

    header布局

--- src/client/components/NavHeader.jsx
+++ src/client/components/NavHeader.jsx
@@ -1,22 +1,12 @@
 
 
 import React from 'react'
-import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { NavBar, Icon } from 'antd-mobile';
-import { login } from '../redux/action/user.action';
-
-@connect(
-    null,
-    {
-        login,
-    }
-)
 
 class NavHeader extends React.Component {
 
     render() {
-        console.log(this.props)
         const { pathname = '/' } = this.props.location
         const name = pathname === '/' || pathname === '/article' ? '文章' :
             pathname === '/record' ? '记录' :
