5c850787c272ae98a2abb6274e0b97170584d8c7
wangsen <295656753@qq.com>
2018-05-31 11:42:37 +0800

    cookie保存和跳转资料页

--- src/client/container/User.jsx
+++ src/client/container/User.jsx
@@ -1,40 +1,34 @@
 import React from 'react'
 import { connect } from "react-redux"
+import cookie from 'react-cookies'
 import axios from 'axios'
 import { Button } from 'antd-mobile';
 
 import Login from '../components/Login'
-// import { demoAction } from '../../redux/action/demo.action'
 import style from './app.less'
 
-
+@connect(
+    state => ({
+        loginStatus: state.userReducer.get('loginStatus'),
+    }),
+    {}
+)
 class User extends React.Component {
-    componentDidMount() {
-        // this.props.demoAction({ name: 'wangsen' })
+    constructor(props) {
+        super(props)
+        this.state = { userid: cookie.load('userid') }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ userid: cookie.load('userid') })
     }
     render() {
-        debugger
-        return (
-            <div>
-                {
-                    this.props.loginStatus ?
-                        <h3>登陆成功，用户资料页面</h3>
-                        : <Login />
-
-                }
-            </div>
-        )
+        const { userid } = this.state
+        if (!userid) {
+            return <Login />
+        }
+        return <h3>登陆成功，用户资料页面</h3>
     }
 }
 
-function mapStateToProps(state) {
-    return {
-        loginStatus: state.userReducer.get('loginStatus'),
-    }
-}
-function mapDispachToProps(dispatch) {
-    return {
-        // demoAction,
-    }
-}
-export default connect(mapStateToProps, mapDispachToProps)(User)
+export default User;
