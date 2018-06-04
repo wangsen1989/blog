ca92346c0ea0931387968b62f7869a0753c9ca17
wangsen <295656753@qq.com>
2018-06-04 18:09:40 +0800

    文章list组件自定滚动上次位置，滚动失败，只滚动到630px左右

--- src/client/container/Article.jsx
+++ src/client/container/Article.jsx
@@ -1,37 +1,14 @@
 import React from 'react'
-import { connect } from "react-redux"
 import ArticleListView from "../components/ArticleListView"
-import { getArticles } from '../redux/action/article.action'
 
 
 
 
-@connect(
-  state => ({
-    $$articles: state.articleReducer.get('$$articles')
-  }),
-  {
-    getArticles,
-  }
-)
 class Article extends React.Component {
-  componentDidMount() {
-    console.log("componentDidMount");
-    // this.props.getArticles()
-  }
+
   render() {
-    const articles = this.props.$$articles.toJS()
     return (
       <div>
-        {/* {
-          articles.map(art => (
-            <div key={art._id}>
-              <p>{art.name}</p>
-              <p>{art.title}</p>
-              <p>{art.content}</p>
-            </div>
-          ))
-        } */}
         <ArticleListView />
       </div>
     )
