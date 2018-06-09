27f4757ec98a79a3138324c9348ae0cce976569f
wangsen <295656753@qq.com>
2018-06-09 18:21:13 +0800

    上传bug

--- src/client/redux/action/user.action.js
+++ src/client/redux/action/user.action.js
@@ -9,72 +9,72 @@ export const LOAD_USER_INFO = 'load user info'
 export const LOGOUT = 'logout'
 
 const lodUserInfo = (payload) => ({
-    type: LOAD_USER_INFO,
-    payload,
+  type: LOAD_USER_INFO,
+  payload,
 })
 
 export const login = (opts) => {
-    return dispatch => {
-        const { name, password } = opts
-        if(!name||!password){
-            Toast.fail('执行失败，请检查填写是否合法', 1);
-            return;
-        }
-        axiosData('/api/login', { name, password: pwdSault(password) })
-            .then(res => {
-                dispatch(lodUserInfo(res.data))
-            })
-            .catch(err => {
-                console.log(err)
-            })
+  return dispatch => {
+    const { name, password } = opts
+    if (!name || !password) {
+      Toast.fail('执行失败，请检查填写是否合法', 1);
+      return;
     }
+    axiosData('/api/login', { name, password: pwdSault(password) })
+      .then(res => {
+        dispatch(lodUserInfo(res.data))
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
 
 
 export const getUserInfo = () => {
-    return dispatch => {
-        return axiosData('/api/userInfo')
-            .then(res => {
-                const { data = {} } = res
-                dispatch(lodUserInfo(data))
-                // return Promise.resolve(data)
-            })
-            .catch(err => {
-                // return Promise.reject(err)
-                console.log(err)
-            })
-    }
+  return dispatch => {
+    return axiosData('/api/userInfo')
+      .then(res => {
+        const { data = {} } = res
+        dispatch(lodUserInfo(data))
+        // return Promise.resolve(data)
+      })
+      .catch(err => {
+        // return Promise.reject(err)
+        console.log(err)
+      })
+  }
 }
 
 export const logOut = () => {
-    return dispatch => {
-        dispatch({
-            type: LOGOUT,
-        })
-    }
+  return dispatch => {
+    dispatch({
+      type: LOGOUT,
+    })
+  }
 }
 
 export const upLoadFile = (file) => {
-    return dispatch => {
-        const formData = new FormData()
-        formData.append('avatar', file)
-        const config = {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        }
-        return Axios.post('/api/upLoadFile', formData, config)
-            .then(res => {
-                if (res.data.code = '000') {
-                    console.log(res.data.data)
-                    return Promise.resolve(res.data.data)
-                } else {
-                    return Promise.reject(res.data.data)
-                }
-            }).catch(err => Promise.reject(err))
-        // dispatch({
-        //     type: LOGOUT,
-        // })
+  return dispatch => {
+    const formData = new FormData()
+    formData.append('avatar', file)
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
     }
+    return Axios.post('/api/upLoadFile', formData, config)
+      .then(res => {
+        if (res.data.code === '000') {
+          console.log(res.data.data)
+          return Promise.resolve(res.data.data)
+        } else {
+          return Promise.reject(res.data.data)
+        }
+      }).catch(err => Promise.reject(err))
+    // dispatch({
+    //     type: LOGOUT,
+    // })
+  }
 }
 
